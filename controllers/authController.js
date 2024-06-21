9f0d245f5bde598f17ab1f8242dcbf14c88819e5
Katapentakill <manesillasdelreloj@gmail.com>
2024-06-20 21:42:23 -0400

    Initial Commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,47 +2,54 @@ const { response, request } = require("express");
 const User = require("../models/user");
 
 const login = async (req = request, res = response) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        // Validate exists email
-        const user = await User.findOne({ where: { email } });
+    // Validar si existe el usuario
+    const user = await User.findOne({ where: { email } });
 
-        if (!user) {
-            return res.status(400).json({
-                success: false,
-                error: true,
-                message: 'Invalid credentials.'
-            });
-        }
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Credenciales inválidas.",
+      });
+    }
 
-        // Assuming your User model has a direct comparison for password
-        if (user.password !== password) {
-            return res.status(400).json({
-                success: false,
-                error: true,
-                message: 'Invalid credentials.'
-            });
-        }
-
-        // If you reach here, credentials are valid
-        const { name, lastName, phone, email: emailUser, image, role_id } = user;
-
-        const dataUser = { id: user.id, name, lastName, phone, email: emailUser, image, role_id };
-
-        return res.status(200).json({
-            success: true,
-            data: dataUser
-        });
-    } catch (error) {
-        return res.status(500).json({
+    // Comparar contraseñas
+    if (user.password !== password) {
+        return res.status(400).json({
             success: false,
             error: true,
-            message: error.message
+            message: 'Credenciales inválidas.',
         });
     }
-}
+
+    // Si las credenciales son válidas
+    const { id, email: userEmail, role_id, nombre, company_id, area_id } = user;
+
+    const dataUser = {
+      id,
+      email: userEmail,
+      role_id,
+      nombre,
+      company_id,
+      area_id,
+    };
+
+    return res.status(200).json({
+      success: true,
+      data: dataUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
-    login,
-}
+  login,
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,7 +12,6 @@ const router = Router();
 router.post('/login', [
     check('email', 'the field email is required').not().isEmpty(),
     check('email', 'this not valid email').isEmail(),
-    check('email', 'the field email is required').custom(verifyEmailLogin),
     check('password', 'the field password is required').not().isEmpty(),
     validateFields
 ], login);
